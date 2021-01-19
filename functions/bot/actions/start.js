f7d3971d560d97e14340864b5973b68202dfaca0
MAX <malkhaz.gholijashvili@gmail.com>
2021-01-19 16:35:39 +0400

    no message

--- functions/bot/actions/start.js
+++ functions/bot/actions/start.js
@@ -2,7 +2,18 @@ const { newUser} = require('../components/fauna')
 const { getUser } = require('../components/helper')
 
 module.exports = async ctx => {
-    return ctx.reply(`გამარჯობა ${name}`)
+  const { id, isBot, name } = getUser(ctx.from)
+
+  if (isBot) {
+    return ctx.reply(`Sorry I only interact with humans!`)
+  }
+
+  try {
+    return ctx.reply(`გამარჯობა ${name}!`)
+  } catch (e) {
+    return ctx.reply(`Error occured`)
+  }
+
 }
 // module.exports = async ctx => {
 //     const { id, isBot, name } = getUser(ctx.from)
