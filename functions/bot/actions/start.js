e6ef5c356cb07859c1557bc2548470d12b5ea163
MAX <malkhaz.gholijashvili@gmail.com>
2021-01-20 13:55:39 +0400

    no message

--- functions/bot/actions/start.js
+++ functions/bot/actions/start.js
@@ -2,39 +2,16 @@ const { newUser} = require('../components/fauna')
 const { getUser } = require('../components/helper')
 
 module.exports = async ctx => {
-  const { id, isBot, name } = getUser(ctx.from)
+    const { id, isBot, name } = getUser(ctx.from)
 
-  if (isBot) {
-    return ctx.reply(`Sorry I only interact with humans!`)
-  }
+    if (isBot) {
+        return ctx.reply(`Sorry I only interact with humans!`)
+    }
   
-  try {
-    setInterval(() => {
-        ctx.reply(`გამარჯობა ${name}!`),
-        1,000})
-    
-  } catch (e) {
-    return ctx.reply(`Error occured`)
-  }
+    try {
+        return ctx.reply(`გამარჯობა ${name}!`) 
+    } catch (e) {
+        return ctx.reply(`Error occured`)
+    }
 
 }
-// module.exports = async ctx => {
-//     const { id, isBot, name } = getUser(ctx.from)
-  
-//     if (isBot) {
-//       return ctx.reply(`Sorry I only interact with humans!`)
-//     }
-  
-//     try {
-//       let isNewUser = await newUser(id)
-//       if (isNewUser) {
-//         return ctx.reply(`Added ${name} to db!`)
-//       }else{
-//         return ctx.reply(`${name} is already inside db!`)
-//       }
-     
-//     } catch (e) {
-//       return ctx.reply(`Error occured`)
-//     }
-  
-//   }
